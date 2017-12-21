127fb5d619db4161825bbd6a2866b343bbb182eb
aarchamb <anthony85180@gmail.com>
2017-12-21 17:58:09 +0100

    Ajout api locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {StoryService} from './story.service';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { StoriesComponent } from './stories/stories.component';
 import { StoryDetailComponent } from './story-detail/story-detail.component';
 import { AppRoutingModule } from './app-routing.module';
-
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StoriesComponent,
-    StoryDetailComponent
+    StoryDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [StoryService],
+  providers: [StoryService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
