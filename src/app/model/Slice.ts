5946ad395000548e00660515b2c50bbb7826820b
aarchamb <anthony85180@gmail.com>
2018-04-12 11:32:13 +0200

    Ajout de pastilles de couleur pour différencier les niveaux des passages

--- src/app/model/Slice.ts
+++ src/app/model/Slice.ts
@@ -8,37 +8,8 @@ export class Slice {
     story: string;
     level: number;
     choices?: Choice[];
+    color?: string;
     constructor() {
     }
-
-    // constructor(title: string, text: string, story: number) {
-    //   this.title = title;
-    //   this.text = text;
-    //   this.story = story;
-    //  }
-
-    // setTitle(title: string) {
-    //   this.title = title;
-    // }
-
-    // setText(text: string) {
-    //   this.text = text;
-    // }
-
-    // setStory(storyId: number) {
-    //   this.story = storyId;
-    // }
-
-    // getTitle() {
-    //   return this.title;
-    // }
-
-    // getStory() {
-    //   return this.story;
-    // }
-
-    // getText() {
-    //   return this.text;
-    // }
 
   }
--- src/app/pipes/slice-by-level.pipe.ts
+++ src/app/pipes/slice-by-level.pipe.ts
@@ -2,12 +2,41 @@ import { Pipe, PipeTransform } from '@angular/core';
 import {Slice} from '../model/Slice';
 @Pipe({
   name: 'sliceByLevel',
-  pure: false
+  pure: true
 })
 export class SliceByLevelPipe implements PipeTransform {
+  allSlice: Slice[];
 
   transform(allSlice: Slice[], number: number): any {
-    return allSlice.filter(slice => slice.level === number);
+    // return allSlice.filter(slice => slice.level === number);
+    return this.filter(allSlice, number);
   }
+
+  /**
+   * Filtrer les passages en fonction de leur niveau,
+   * pour chaque niveau on attribue une couleur aléatoire au apssage
+   * @param allSlice
+   * @param number
+   */
+  filter(allSlice, number) {
+    this.allSlice = allSlice.filter(slice => slice.level === number);
+    if (this.allSlice.length === 0) {return this.allSlice; }
+
+    const randomColor = this.color();
+    for (const slice of this.allSlice) {
+      slice.color = randomColor;
+    }
+
+    return this.allSlice;
+  }
+
+  /**
+   * Genère un code couleur aléatoire
+   */
+    color(): string {
+        return'rgb(' + Math.floor(Math.random() * 255)
+        + ',' + Math.floor(Math.random() * 255) + ','
+        + Math.floor(Math.random() * 255) + ')';
+    }
 
 }
